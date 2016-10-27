efe6b3479d91ca7aae80025afb89d953a861c72c
Arunabh <arunabhghosh98@gmail.com>
2016-10-28 02:19:35 +0530

    convert audio to amplitude signals and pass them in chunks

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 import requests
+import wavefile
+import sys
 
-with open('music/test1.wav', mode='rb') as file:
-    fileContent = file.read()
-    for line in fileContent:
-        r = requests.post('http://192.168.0.112/login', data = {'CHARACTER':line})
+# returns the contents of the wav file as a double precision float array
+def wav_to_floats(filename = 'test1.wav'):
+    w = wavefile.load(filename)
+    return w[1][0]
+
+signal = wav_to_floats('test1.wav')
+
+def chunks(l, n):
+    n = max(1, n)
+    return [l[i:i+n] for i in xrange(0, len(l), n)]
+
+chunked_signals = chunks(signal, 1000)
+
+for chunked_signal in chunked_signals:
+    r = requests.post('http://192.168.0.112/login', data = {'CHARACTER':str(chunked_signal)})
